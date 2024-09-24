e407c78899c98f3a363ca3b4c390a0066ca6fc75
Simon Fischer <mail@simonfischer.info>
2024-09-24 19:16:18 +0200

    Initial Commit

---
+++ include/Lua.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <memory>
+#include <vector>
+#include <Print.h>
+
+extern "C" {
+    struct lua_State;
+}
+
+class Lua {
+public:
+    explicit Lua();
+
+    void run(const char* script, Print* output = nullptr);
+
+    void load(const char* fileName, Print* output = nullptr);
+
+    void call(const char* func, const std::vector<std::string>& argv, Print* output = nullptr);
+
+    void addModule(const char* name, int(*loadFunc)(lua_State*));
+
+private:
+    lua_State* m_state;
+};
